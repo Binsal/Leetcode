7ab8b122d1815604592675a132ee6dda610ecd8d
Binsal <96328531+Binsal@users.noreply.github.com>
2022-12-30 10:46:20 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (27.00%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       
+        vector<int>dp(nums.size()+2,0);
+        
+        for(int i=nums.size()-1;i>=0;i--){
+            dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
+        }
+        
+        return dp[0];
+    }
+};
