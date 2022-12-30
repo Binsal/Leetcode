8961e6428b18bba0de3e6cb35d462a555180b57c
Binsal <96328531+Binsal@users.noreply.github.com>
2022-12-30 08:33:24 +0530

    Time: 4 ms (77.83%), Space: 12.2 MB (38.04%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-//        int ind=-1;
-//         if(nums.size()==1)return;
+       int ind=-1;
+        if(nums.size()==1)return;
         
-//         for(int i=nums.size()-2;i>=0;i--){
-//             if(nums[i]<nums[i+1]){
-//                 swap(nums[i],nums[nums.size()-1]);
-//                 ind=i;
-//                 break;
-//             }
-//         }
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
                
-//         if(ind==0){
-//         reverse(nums.begin(),nums.end());
-//         }
-//         else{
-//             int ind2=0;
-//             for(int i=nums.size()-1;i>=0;i--){
-//                 if(nums[i]>nums[ind]){
-//                     ind=i;
-//                 }
-//             }
+        if(ind==-1){
+        reverse(nums.begin(),nums.end());
+        }
+        else{
+            int ind2=0;
+            for(int i=nums.size()-1;i>=0;i--){
+                if(nums[i]>nums[ind]){
+                    ind2=i;
+                    break;
+                }
+            }
             
-//             swap(nums[ind],nums[ind2]);
-//             sort(nums.begin()+ind,nums.end());
-//         }
+            swap(nums[ind],nums[ind2]);
+            sort(nums.begin()+ind+1,nums.end());
+        }
         
-        next_permutation(nums.begin(),nums.end());
+        //next_permutation(nums.begin(),nums.end());
     }
 };
