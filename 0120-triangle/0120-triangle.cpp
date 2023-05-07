d03775a41ca8c4fb33fe36bae4a5b4c84d963c0a
Binsal <96328531+Binsal@users.noreply.github.com>
2023-05-07 06:11:49 +0530

    Time: 7 ms (63.85%), Space: 8.6 MB (94.11%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int ans=0;
+       
+        vector<int>dp(triangle.back());
+        
+        for(int i=triangle.size()-2;i>=0;i--){
+               for(int j=0;j<=i;j++){
+                   dp[j]=min(dp[j],dp[j+1])+triangle[i][j];
+               }
+        }
+            
+        return dp[0];
+    }
+};
