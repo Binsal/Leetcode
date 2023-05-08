af7f8463a4aa1ec7c544c37bf696f328625a1b7d
Binsal <96328531+Binsal@users.noreply.github.com>
2023-05-08 09:07:06 +0530

    Time: 44 ms (98.45%), Space: 42.2 MB (37.61%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    var mappedArray=[];
+    
+    for(var i=0;i<arr.length;i++){
+        mappedArray.push(fn(arr[i],i));
+    }
+    
+    return mappedArray;
+};
+
