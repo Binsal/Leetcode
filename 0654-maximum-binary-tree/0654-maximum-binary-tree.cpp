b722dbce05d2d1772633f4c23709fb497d560104
Binsal <96328531+Binsal@users.noreply.github.com>
2023-05-19 10:39:27 +0530

    Time: 76 ms (91.74%), Space: 39.3 MB (98.28%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
@@ -12,19 +12,23 @@
 class Solution {
 public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        vector<TreeNode*> stk;
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            TreeNode* cur = new TreeNode(nums[i]);
-            while (!stk.empty() && stk.back()->val < nums[i])
-            {
-                cur->left = stk.back();
+        vector<TreeNode*>stk;
+        
+        for(int i=0;i<nums.size();i++){
+            TreeNode *curr=new TreeNode(nums[i]);
+            
+            while(!stk.empty() && stk.back()->val<nums[i]){
+                curr->left=stk.back();
                 stk.pop_back();
             }
-            if (!stk.empty())
-                stk.back()->right = cur;
-            stk.push_back(cur);
+            
+            if(!stk.empty()){
+                stk.back()->right=curr;
+            }
+            
+            stk.push_back(curr);
         }
-        return stk.front();
+        
+        return stk[0];
     }
 };
