eee1a006afa23d8f0c9092aa6f9aef942a62e0cb
Binsal <96328531+Binsal@users.noreply.github.com>
2023-05-26 14:40:34 +0530

    Time: 127 ms (53.74%), Space: 28.9 MB (65.66%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -2,26 +2,15 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         int steps=0;
-        string str1;
-        string str2;
-        
-        if(s.size()<t.size()){
-            str1=s;
-            str2=t;
-        }
-        else{
-            str2=t;
-            str1=s;
-        }
         
         vector<int>vec(26,0);
         
-        for(int i=0;i<t.size();i++){
-            vec[t[i]-'a']++;
+        for(int i=0;i<s.size();i++){
+            vec[s[i]-'a']++;
         }
         
-        for(int i=0;i<s.size();i++){
-            vec[s[i]-'a']--;
+        for(int i=0;i<t.size();i++){
+            vec[t[i]-'a']--;
         }
         
         for(int i=0;i<26;i++)steps+=abs(vec[i]);
