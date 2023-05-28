5c09860ab9df983ac21d17ce95bd63ac4decd834
Binsal <96328531+Binsal@users.noreply.github.com>
2023-05-28 10:01:01 +0530

    Time: 27 ms (100.00%), Space: 12.3 MB (100.00%) - LeetHub

---
+++ 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    
+    long long minimumCost(string s) {
+        long long cost=0;
+        int n=s.size();
+        
+            for(int i=1;i<s.size();i++){
+                if(s[i]!=s[i-1])cost+=min(i,n-i);
+            }
+        
+        return cost;
+    }
+};
+
