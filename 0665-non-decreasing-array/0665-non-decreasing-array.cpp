edc83a94bd014f4c5e028912a7f640ee23eda8af
Binsal <96328531+Binsal@users.noreply.github.com>
2023-05-29 22:54:24 +0530

    Time: 32 ms (45.96%), Space: 26.9 MB (80.51%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-  bool checkPossibility(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1)return true;
-        vector<int> prefix(n,false);
-        vector<int> suffix(n,false);
-        prefix[0] = true;
-        for(int i=1;i<n;i++)
-        {
-            if(nums[i]>=nums[i-1] and prefix[i-1])prefix[i] = true;
+    bool checkPossibility(vector<int>& nums) {
+        int cnt = 0;                                                                    //the number of changes
+        for(int i = 1; i < nums.size() && cnt<=1 ; i++){
+            if(nums[i-1] > nums[i]){
+                cnt++;
+                if(i-2<0 || nums[i-2] <= nums[i])nums[i-1] = nums[i];                    //modify nums[i-1] of a priority
+                else nums[i] = nums[i-1];                                                //have to modify nums[i]
+            }
         }
-        
-        suffix[n-1] = true;
-        for(int i=n-2;i>=0;i--)
-        {
-            if(nums[i]<=nums[i+1] and suffix[i+1])suffix[i] = true;
-        }
-        
-        if(prefix[n-2] or suffix[1])return true;
-        for(int i=1;i<n-1;i++)
-        {
-            if(prefix[i-1] and suffix[i+1] and nums[i-1]<=nums[i+1])return true;
-        }
-        return false;
-    }
+        return cnt<=1;
+    } 
 };
