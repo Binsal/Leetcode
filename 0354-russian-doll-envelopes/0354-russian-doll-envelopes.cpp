20070e50583eacc268aba727949408f985c38aa9
Binsal <96328531+Binsal@users.noreply.github.com>
2023-06-26 08:27:00 +0530

    Time: 468 ms (22.43%), Space: 87.7 MB (22.29%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     static bool comp(vector<int> &a, vector<int> &b){
-        if(a[0]==b[0]){
-            return a[1]>b[1];
-        }
+        if(a[0]==b[0])return a[1]>b[1];
         return a[0]<b[0];
     }
         
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(), envelopes.end(), comp);
-        int i, j, n=envelopes.size();
-        vector<int>lis;
         
-        for(i=0; i<n; i++){
-            auto it = lower_bound(lis.begin(), lis.end(), envelopes[i][1]);
-            if(it==lis.end()){lis.push_back(envelopes[i][1]);}
+        vector<int>v;
+        
+        for(auto vec:envelopes){
+            
+            if(v.empty() || (vec[1]>v.back())){
+                v.push_back(vec[1]);
+            }
             else{
-                *it = envelopes[i][1];
+        
+               int ind=lower_bound(v.begin(),v.end(),vec[1])-v.begin(); 
+               v[ind]=vec[1]; 
             }
         }
-        return lis.size();
+        
+        return v.size();
     }
 };
