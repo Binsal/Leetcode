6642b6a6c0dfe1030a4fb90500b50ae9325fee6e
Binsal <96328531+Binsal@users.noreply.github.com>
2023-07-21 23:52:44 +0530

    Time: 28 ms (40.74%), Space: 26 MB (80.77%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,16 +11,17 @@
  */
 class Solution {
 public:
-    TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b){
-        if(x > y || a > b)return nullptr;
-        TreeNode *node = new TreeNode(po[b]);
-        int SI = x;  
-        while(node->val != in[SI])SI++;
-        node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
-        node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
-        return node;
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = size(inorder), postIdx = n-1;
+        return build(inorder, postorder, 0, n-1, postIdx);
     }
-    TreeNode* buildTree(vector<int>& in, vector<int>& po){
-        return Tree(in,0,in.size()-1,po,0,po.size()-1);
+
+    TreeNode* build(vector<int>& in, vector<int>& post, int inStart, int inEnd, int& postIdx) {
+        if(inStart > inEnd) return nullptr;
+        TreeNode* root = new TreeNode(post[postIdx--]);
+        int inIdx = find(begin(in), end(in), root -> val) - begin(in);
+        root -> right = build(in, post, inIdx+1, inEnd, postIdx);
+        root -> left  = build(in, post, inStart, inIdx-1, postIdx);
+        return root;
     }
 };
