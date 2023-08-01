a14c342dfbb137257ef34ccc08ffb631609f49b1
Binsal <96328531+Binsal@users.noreply.github.com>
2023-08-01 12:47:01 +0530

    Time: 19 ms (91.50%), Space: 11.1 MB (41.41%) - LeetHub

---
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long countVowels(string word) {
+        long long count=0;
+       
+        for(int i=0;i<word.size();i++){
+            
+            if(word[i]=='a'|| word[i]=='e'|| word[i]=='i' || word[i]=='o' || word[i]=='u')
+            {
+                count+=(word.size()-i)*(i+1);
+            }
+        }
+        
+        return count;
+    }
+};
