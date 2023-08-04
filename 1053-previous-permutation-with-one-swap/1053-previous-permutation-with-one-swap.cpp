4e3c38e06282545a52f8df0d956696a93e6f022f
Binsal <96328531+Binsal@users.noreply.github.com>
2023-08-04 23:16:59 +0530

    Time: 30 ms (64.34%), Space: 24.7 MB (60.84%) - LeetHub

--- 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.cpp
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     vector<int> prevPermOpt1(vector<int>& arr) {
         int n=arr.size();
-        int left = -1;
         
-        for(int i = n-1; i>0; i--){
-            if(arr[i-1]>arr[i]){
-                left = i-1;
-                break;
+        int left=-1;
+        
+        for(int i=n-1;i>0;i--){
+            
+            if(arr[i-1]>arr[i]){left=i-1;
+                                break;
+                               }
+        }
+        
+        if(left==-1)return arr;
+        
+        int right=left+1;
+        
+        for(int j=left+1;j<n;j++){
+            if(arr[j]<arr[left] && arr[right]<arr[j]){
+                right=j;
             }
         }
         
-        if(left == -1)
-            return arr;
-        
-        int right = left+1;
-        
-        for(int i = left+1; i<n; i++){
-            if(arr[i]< arr[left] && arr[right] < arr[i]) 
-                right = i;
-        }
         
         swap(arr[left],arr[right]);
         
         return arr;
-
     }
 };
