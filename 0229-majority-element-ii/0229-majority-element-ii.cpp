24fb87de31482dfcadb058c00f849b06ed33ed05
Binsal <96328531+Binsal@users.noreply.github.com>
2023-10-05 16:58:09 +0530

    Time: 15 ms (14.75%), Space: 16.2 MB (28.20%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,20 +2,28 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int>v;
+        int y(-1),z(-1),cy(0),cz(0);
         
-        map<int,int>mp;
+        
+            for(auto x:nums){
+                if(x==y)cy++;
+                else if(x==z)cz++;
+                else if(!cy){y=x;cy=1;}
+                else if(!cz){z=x;cz=1;}
+                else {cy--;cz--;}
+            }
+      
+        
+        cy=cz=0;
         
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-            
-            
+            if(nums[i]==y)cy++;
+            else if(nums[i]==z)cz++;
         }
         
-        for(auto x:mp){
-            if(x.second>floor(nums.size()/3.0)){
-                v.push_back(x.first);
-            }
-        }
+        
+        if(cy>nums.size()/3)v.push_back(y);
+        if(cz>nums.size()/3)v.push_back(z);
         
         return v ;
     }
