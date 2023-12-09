fb6937d10325277a636d9f0511570ba3f87fedba
Saloni Bindal <96328531+Binsal@users.noreply.github.com>
2023-12-09 20:34:36 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (81.67%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -20,29 +20,27 @@ public:
         TreeNode *curr=root;
         
         while(curr!=NULL){
-            
-            if(curr->left==NULL){
-                v.push_back(curr->val);
-                curr=curr->right;
-            }
-            else{
-                TreeNode *prev=curr->left;
-                
-                while(prev->right && prev->right!=curr){
-                    prev=prev->right;
-                }
-                
-                if(prev->right==NULL){
-                    prev->right=curr;
-                    curr=curr->left;
-                }
-                else{
-                    prev->right=NULL;
-                    v.push_back(curr->val);
-                    curr=curr->right;
-                }
-                
-            }
+           if(curr->left==NULL){
+               v.push_back(curr->val);
+               curr=curr->right;
+           }
+           else{
+               TreeNode *prev=curr->left;
+               
+               while(prev->right && prev->right!=curr){
+                   prev=prev->right;
+               }
+               
+               if(prev->right==NULL){
+                   prev->right=curr;
+                   curr=curr->left;
+               }
+               else{
+                   prev->right=NULL;
+                   v.push_back(curr->val);
+                   curr=curr->right;
+               }
+           }
         }
         
         
