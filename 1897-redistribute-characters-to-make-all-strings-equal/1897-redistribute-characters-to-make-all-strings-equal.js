0a8809277bbd530e8f333942d3e23ca746a54e53
Saloni Bindal <96328531+Binsal@users.noreply.github.com>
2023-12-30 08:44:59 +0530

    Time: 67 ms (70.42%), Space: 45 MB (46.48%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string[]} words
+ * @return {boolean}
+ */
+var makeEqual = function(words) {
+//     let counts = new Map();
+    
+//     for (let word of words) {
+//         for (let c of word) {
+//             counts.set(c, (counts.get(c) || 0) + 1);
+//         }
+//     }
+    
+//     let n = words.length;
+//     for (let val of counts.values()) {
+//         if (val % n !== 0) {
+//             return false;
+//         }
+//     }
+    
+//     return true;
+    
+    let counts=new Map();
+    
+    for(let word of words){
+        for(let c of word){
+            counts.set(c,(counts.get(c)||0)+1);
+        }
+    }
+    
+    let n =words.length;
+    
+    for (let val of counts.values()){
+        if(val%n!==0){
+            return false;
+        }
+    }
+    
+    return true;
+};
